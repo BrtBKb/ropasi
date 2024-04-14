960cd62fb27f298b4e785ccea12d703bcaa7115f
Bryantken <132709114+BrtBKb@users.noreply.github.com>
2024-04-14 18:09:01 -0400

    Updated js file Second version is close to the objectives of the project. Except the loop isn't working well. Review material, come back to fix this.

--- script.js
+++ script.js
@@ -5,41 +5,58 @@ let player;             //declared player for prompt's input
     }  
 let arrRandom = ["rock", "paper", "scissor"];     //create an array for the computer to choose its three choices.
 let computer = arrRandom[(Math.floor(Math.random() * arrRandom.length))];  // SET computer's input to be random using array's index
-let userCount = 0;                         //declared a variable to tally user's win
-let computerCount = 0 ;                     //declared a variable to tally computer's win
-function playRound (playerSelection, computerSelection){   // OBTAIN inputs from player and computers
+
+
+
+function play(playerSelection, computerSelection){   // OBTAIN inputs from player and computers
          computerSelection = computerSelection.toLowerCase();   //all string will be lower case with this method. This method will help further later when comparing strings. 
          playerSelection = playerSelection.toLowerCase();       //player's input will format strings into lowercase.
-            if(playerSelection === computerSelection){           //compare both input, using an if statement. if both are the same it will be a tie
-                 userCount += 1;                               //increment the user tally to one
-                 computerCount += 1;                           //increment the computer tally to one
-                 alert("tie");
-            }
-          else if(playerSelection === "rock" &&  computerSelection === "scissor"){    //from here down it will go over the rules of the game.
-             userCount += 1;                                                          //add an increment to whichever wins inside the condition
-             alert("You win");                                                        //temp solution to see who is winning when trying it on the google chrome browser
-          }
-         else if(playerSelection === "paper" &&  computerSelection === "rock"){
-             userCount += 1;
-             alert("you win");
-            }
-          else if(playerSelection === "scissor" &&  computerSelection === "paper"){
-             userCount += 1;
-             alert("you win");
-            }
-          else if(playerSelection === "rock" &&  computerSelection === "paper"){
-             computerCount += 1;
-             alert("you lose");
-            }
-          else if(playerSelection === "paper" &&  computerSelection === "scissor"){
-             computerCount += 1;
-             alert("you lose");
-            }
-          else if(playerSelection === "scissor" &&  computerSelection === "rock"){
-             computerCount += 1;
-             alert("you lose");
-            }
+         let userCount = 0;                         //declared a variable to tally user's win
+         let computerCount = 0 ;                     //declared a variable to tally computer's win
+         let result ;
+         
+         switch(true){
+         
+         case playerSelection === "rock" &&  computerSelection === "scissor":
+         case playerSelection === "paper" &&  computerSelection === "rock":
+         case playerSelection === "scissor" &&  computerSelection === "paper":
+         userCount += 1;
+         result = "User win";
+         break;
+         case computerSelection === "rock" &&  playerSelection === "scissor":
+         case computerSelection === "paper" &&  playerSelection === "rock":
+         case computerSelection === "scissor" &&  playerSelection === "paper":
+         computerCount += 1;
+         result = "computer win";
+         break;
+         default:        //compare both input, using an if statement. if both are the same it will be a tie  
+         userCount += 1;                               //increment the user tally to one
+         computerCount += 1;                           //increment the computer tally to one
+         result = "tie";
+         break;
         }
-playRound(player,computer);                                                 //add the two variable I declared in the beginning as the arguments. These variable has stored user's input and computer's input
+        return {userCount, computerCount, result};
+      }
+        
+
+    function playRound(playerSelection) {
+        let userCount = 0;
+        let computerCount = 0;
+        let result;
+        for (let round = 0; round <= 5; round++) {
+            let arrRandom = ["rock", "paper", "scissor"];
+            let computerSelection = arrRandom[Math.floor(Math.random() * arrRandom.length)];
+            let roundResult = play(playerSelection, computerSelection);
+            userCount += roundResult.userCount;
+            computerCount += roundResult.computerCount;
+            result = roundResult.result;
+        }
+        alert(`After 5 rounds: ${result}, User: ${userCount}, Computer: ${computerCount}`);
+    }
+    
+
+        
+              
+playRound(player);                                                 //add the two variable I declared in the beginning as the arguments. These variable has stored user's input and computer's input
 
 
